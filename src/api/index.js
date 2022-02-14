5281e3d334918b7ce7ef997cde7c318041bb50b1
Kamipown <paul.delobbel@gmail.com>
2022-02-14 18:36:32 +0100

    refactor: api post get functions

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,25 @@
+const headers = {
+  "Content-type": "application/json"
+}
+
+const get = async (uri, params) => {
+  let path = window.location.pathname + uri
+  if (params)
+    path += '?' + (new URLSearchParams(params)).toString()
+  const response = await fetch(path, { method: "GET", headers })
+  return response
+}
+
+const post = async (uri, data) => {
+  const path = window.location.pathname + uri
+  const body = JSON.stringify(data)
+  const response = await fetch(path, { method: "POST", headers, body })
+  return response.json()
+}
+
 const api = {
   getJSON: async name => {
-    const response = await fetch(`${window.location.pathname}/jsons/${name}.json`, {
-      method: "GET",
-      headers: {
-        "Content-type": "application/json"
-      }
-    })
+    const response = await get(`/jsons/${name}.json`)
     const json = await response.json()
     return (json)
   }
